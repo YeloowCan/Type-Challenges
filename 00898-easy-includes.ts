1178dae88d2baa9b20fb226190c0c119bff9e2f9
YeloowCan <942789838@qq.com>
2023-04-04 21:24:01 +0800

    include

--- 00898-easy-includes.ts
+++ 00898-easy-includes.ts
@@ -1,9 +1,13 @@
 // ============= Test Cases =============
-import type { Equal, Expect } from './test-utils'
+import type { Equal, Expect } from './test-utils';
 
 type cases = [
-  Expect<Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Kars'>, true>>,
-  Expect<Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'>, false>>,
+  Expect<
+    Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Kars'>, true>
+  >,
+  Expect<
+    Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'>, false>
+  >,
   Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 7>, true>>,
   Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 4>, false>>,
   Expect<Equal<Includes<[1, 2, 3], 2>, true>>,
@@ -17,9 +21,12 @@ type cases = [
   Expect<Equal<Includes<[1], 1 | 2>, false>>,
   Expect<Equal<Includes<[1 | 2], 1>, false>>,
   Expect<Equal<Includes<[null], undefined>, false>>,
-  Expect<Equal<Includes<[undefined], null>, false>>,
-]
-
+  Expect<Equal<Includes<[undefined], null>, false>>
+];
 
 // ============= Your Code Here =============
-type Includes<T extends readonly any[], U> = 
+type Includes<T extends readonly any[], U> = T extends [infer A, ...infer B]
+  ? Equal<A, U> extends true
+    ? true
+    : Includes<B, U>
+  : false;
