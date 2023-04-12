5cab2183a7b09093f47545ff1cd65e6d8782c37e
YeloowCan <942789838@qq.com>
2023-04-12 22:16:21 +0800

    merge

---
+++ 00599-medium-merge.ts
@@ -0,0 +1,29 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils';
+
+type Foo = {
+  a: number;
+  b: string;
+};
+type Bar = {
+  b: number;
+  c: boolean;
+};
+
+type cases = [
+  Expect<
+    Equal<
+      Merge<Foo, Bar>,
+      {
+        a: number;
+        b: number;
+        c: boolean;
+      }
+    >
+  >
+];
+
+// ============= Your Code Here =============
+type Merge<F, S, O = F & S> = {
+  [T in keyof O]: T extends keyof S ? S[T] : O[T];
+};
